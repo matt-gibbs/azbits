e3cd8018e5c9f47ab55645c270bf52115448c760
Matt Gibbs <mattgibbs-msft@outlook.com>
2015-10-01 00:09:11 -0700

    example token credentials

---
+++ src/csharp/AzurePasswordTokenCredentials.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Rest;
+
+namespace AzBits.Rest
+{
+    internal class AzurePasswordTokenCredentials : ServiceClientCredentials
+    {
+        private const string AzureAdAuthority = "https://login.windows.net/";
+        private const string AzureRmResource = "https://management.azure.com/";
+        private readonly string _applicationId;
+        private readonly string _subscriptionId;
+        private readonly string _password;
+        private readonly string _tenantId;
+
+        public AzurePasswordTokenCredentials(
+            string tenantId,
+            string subscriptionId,
+            string applicationId,
+            string password)
+        {
+            _tenantId = tenantId;
+            _subscriptionId = subscriptionId;
+            _applicationId = applicationId;
+            _password = password;
+            TokenExpires = new DateTimeOffset(DateTime.UtcNow);
+        }
+
+        public string Token { get; set; }
+
+        public DateTimeOffset TokenExpires { get; set; }
+
+        public async Task AcquireTokenAsync()
+        {
+            string authority = AzureAdAuthority + _tenantId;
+            var authenticatonContext = new AuthenticationContext(authority,
+                tokenCache: null,
+                validateAuthority: true);
+
+            var clientCredential = new ClientCredential(_applicationId, _password);
+
+            AuthenticationResult result =
+                await authenticatonContext.AcquireTokenAsync(AzureRmResource, clientCredential);
+
+            Token = result.AccessToken;
+            TokenExpires = result.ExpiresOn;
+        }
+
+        public void AcquireToken()
+        {
+            var task = AcquireTokenAsync();
+            task.Wait();
+        }
+
+        public string SubscriptionId
+        {
+            get { return _subscriptionId; }
+        }
+
+        public override Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException("request");
+            }
+            if ((Token == null) || (TokenExpires < new DateTimeOffset(DateTime.UtcNow)))
+            {
+                AcquireToken();
+            }
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", Token);
+            return base.ProcessHttpRequestAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ src/csharp/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.Resources;
+using Microsoft.Azure.Management.Resources.Models;
+using Microsoft.Rest.Azure;
+using AzBits.Rest;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        #region GUIDS
+        //replace subscriptionId and tenantID with valid values
+        const string subscriptionId = "12345678-90ab-cdef-1234-567890abcdef"; 
+        const string tenantId = "12345678-90ab-cdef-1234-567890abcdef";
+        //from ../New-AzureServicePrincipalOwner.ps1
+        const string applicationId = "12345678-90ab-cdef-1234-567890abcdef";
+        const string password = "ExamplePassword";
+
+        #endregion
+
+        public void Main(string[] args)
+        {
+            var credentials = new AzurePasswordTokenCredentials(tenantId, subscriptionId, applicationId, password);
+            var subClient = new SubscriptionClient(credentials);
+            var armClient = new ResourceManagementClient(credentials);
+
+            //BUG: generated operations validate that the client.SubscriptionId != null
+            subClient.SubscriptionId = subscriptionId;
+            armClient.SubscriptionId = subscriptionId;
+
+            TenantListResult tenantListResult = subClient.Tenants.List();
+            IList<TenantIdDescription> tenants = tenantListResult.Value;
+            Console.WriteLine("TENANTS");
+            foreach(TenantIdDescription tenant in tenants)
+            {
+                Console.WriteLine($"Id = {tenant.Id}");
+                Console.WriteLine($"TenantId = {tenant.TenantId}");
+                Console.WriteLine("-----");
+            }
+
+            SubscriptionListResult subListResult = subClient.Subscriptions.List();
+            IList<Subscription> subscriptions = subListResult.Value;
+            Console.WriteLine("SUBSCRIPTIONS");
+            foreach(Subscription sub in subscriptions)
+            {
+                Console.WriteLine($"Displayname = {sub.DisplayName}");
+                Console.WriteLine($"Id = {sub.Id}");
+                Console.WriteLine($"SubscriptionId = {sub.SubscriptionId}");
+                Console.WriteLine($"State = {sub.State}");
+                Console.WriteLine("-----");
+            }
+
+            List<Provider> providers = new List<Provider>();
+            Page<Provider> page = armClient.Providers.List();
+            foreach(Provider p in page)
+            {
+                providers.Add(p);
+            }
+            while(page.NextPageLink != null)
+            {
+                page = armClient.Providers.ListNext(page.NextPageLink);
+                foreach(Provider p in page)
+                {
+                    providers.Add(p);
+                }
+            }
+
+            Console.WriteLine("PROVIDERS");
+            foreach(Provider p in providers)
+            {
+                if(p.Id.EndsWith("Microsoft.MobileEngagement"))
+                Console.WriteLine($"Id = {p.Id}");
+                {
+                    Console.WriteLine($"RegistrationState = {p.RegistrationState}");
+                    Console.WriteLine($"Namespace = {p.NamespaceProperty}");
+                    IList<ProviderResourceType> resourceTypes = p.ResourceTypes;
+                    foreach (ProviderResourceType prt in resourceTypes)
+                    {
+                        Console.WriteLine($"ResourceType = {prt.ResourceType}");
+                        foreach (string v in prt.ApiVersions)
+                        {
+                            Console.WriteLine($"Version = {v}");
+                        }
+                        foreach (string x in prt.Locations)
+                        {
+                            Console.WriteLine($"Location = {x}");
+                        }
+                        IDictionary<string, string> properties = prt.Properties;
+                        if(properties != null)
+                        {
+                            ICollection<string> keys = properties.Keys;
+                            foreach (string k in keys)
+                            {
+                                Console.WriteLine($"{k} = {properties[k]}");
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine("-----");
+            }
+
+            Page<GenericResource> resourcesPage = armClient.Resources.List();
+            foreach(GenericResource resource in resourcesPage)
+            {
+                Console.WriteLine($"name = {resource.Name}");
+                Console.WriteLine($"id = {resource.Id}");
+            }
+
+            Console.WriteLine("<Enter> to continue");
+            Console.ReadLine();
+
+        }
+    }
+}
